2dcb8208ddfd2391f1aafa4bcc593f9e5a4db3ea
ferv95 <fernandovado95@gmail.com>
2018-05-31 11:45:02 +0200

    Add project files.

---
+++ GeneracionDinamica/GeneracionDinamica/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GeneracionDinamica
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            this.btnGenerar.Clicked += BtnGenerar_Clicked;
+		}
+
+        private void BtnGenerar_Clicked(object sender, EventArgs e)
+        {
+            int numBotones = int.Parse(this.entryNumBotones.Text);
+            this.layourContenedor.Children.Clear();
+            for(int i = 1; i <= numBotones; i++)
+            {
+                Button btn = new Button();
+                btn.Text = "Botón " + i;
+                this.layourContenedor.Children.Add(btn);
+            }
+        }
+    }
+}
